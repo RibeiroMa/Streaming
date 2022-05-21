caf75cd0e83e0e9fd1b8166f4424d4221913c02b
RibeiroMa <93168615+RibeiroMa@users.noreply.github.com>
2022-05-20 23:28:52 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+let songs = [{
+        name: 'Always You',
+        path: 'assets/musics/Song 1.mp3',
+        artist: 'Amber Pacific',
+        cover: 'assets/images/cover 1.png'
+    },
+    {
+        name: 'Breathing',
+        path: 'assets/musics/Song 2.mp3',
+        artist: 'Yellowcard',
+        cover: 'assets/images/cover 2.png'
+    },
+    {
+        name: 'Make A Sound',
+        path: 'assets/musics/Song 3.mp3',
+        artist: 'Autopilot Off',
+        cover: 'assets/images/cover 3.png'
+    },
+    {
+        name: 'Orpheus',
+        path: 'assets/musics/Song 4.mp3',
+        artist: 'Ash',
+        cover: 'assets/images/cover 4.png'
+    },
+    {
+        name: 'Animal',
+        path: 'assets/musics/Song 5.mp3',
+        artist: 'Mudmen',
+        cover: 'assets/images/cover 5.png'
+    },
+    {
+        name: 'Lazy Generation',
+        path: 'assets/musics/Song 6.mp3',
+        artist: 'The F-Ups',
+        cover: 'assets/images/cover 6.png'
+    },
+    {
+        name: 'I Wanna Sedated',
+        path: 'assets/musics/Song 7.mp3',
+        artist: 'Ramones',
+        cover: 'assets/images/cover 7.png'
+    },
+    {
+        name: 'Reinventing The Whell To Run Myself Over',
+        path: 'assets/musics/Song 8.mp3',
+        artist: 'Fall Out Boys',
+        cover: 'assets/images/cover 8.png'
+    },
+]
